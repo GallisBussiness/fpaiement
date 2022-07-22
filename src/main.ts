657d189150848e3cf13fc20a581914b3195ddbd2
Mamadou Sy <bussinessgallis@gmail.com>
2022-07-22 10:36:08 +0000

    fix port

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
 
-const PORT = 80;
+const PORT = parseInt(process.env.PORT, 10) || 80;
 const options: MicroserviceOptions = {
   transport: Transport.TCP,
-  options: { port: parseInt(process.env.PORT, 10) || PORT },
+  options: { port: PORT },
 };
 const logger = new Logger();
 
